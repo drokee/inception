b24ecad90e5c9e2c539f57eed4e0854ff766a854
DROKEE <drokee@DROKEEs-MacBook-Air.local>
2024-07-02 12:02:30 +0100

    sec

---
+++ project/srcs/requirements/mariadb/conf/create_db.sh
@@ -0,0 +1,8 @@
+
+CREATE DATABASE IF NOT EXISTS mine;
+USE mine;
+
+CREATE USER 'amahdiou'@'localhost' IDENTIFIED BY '0800';
+
+GRANT ALL PRIVILEGES ON amahdiou.* TO 'amahdiou'@'localhost';
+FLUSH PRIVILEGES;
